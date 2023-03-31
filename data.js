694991d95096f8eeb05ee043370659451135d882
J4ngel <andres_angel@live.com.ar>
2023-03-31 12:16:29 -0500

    Carga inicial de los archivos requeridos

---
+++ data.js
@@ -0,0 +1,277 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entrada-y-pasillo",
+      "name": "Entrada y pasillo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.25711431318545763,
+          "pitch": 0.8456601619628241,
+          "rotation": 0,
+          "target": "1-entrada-del-laboratorio"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-entrada-del-laboratorio",
+      "name": "Entrada del laboratorio",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1682905293180958,
+          "pitch": 0.7707233870769219,
+          "rotation": 0.7853981633974483,
+          "target": "2-laboratorio11"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-laboratorio11",
+      "name": "Laboratorio1(1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.016325694044534,
+          "pitch": 0.8038795555039933,
+          "rotation": 6.283185307179586,
+          "target": "3-laboratorio22"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-laboratorio22",
+      "name": "Laboratorio2(2)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.154123909101008,
+          "pitch": 0.9359732989872036,
+          "rotation": 0,
+          "target": "4-laboratorio33"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-laboratorio33",
+      "name": "Laboratorio3(3)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.12959832292020934,
+          "pitch": 1.0007558695489802,
+          "rotation": 1.5707963267948966,
+          "target": "5-laboratorio44"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-laboratorio44",
+      "name": "Laboratorio4(4)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.07494709388151222,
+          "pitch": 1.0068217991175015,
+          "rotation": 0,
+          "target": "6-laboratorio55"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-laboratorio55",
+      "name": "Laboratorio5(5)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.08762392199194835,
+          "pitch": 0.9172314489447082,
+          "rotation": 1.5707963267948966,
+          "target": "6-laboratorio55"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Laboratorio 5 piso",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
